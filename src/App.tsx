aa53ab1522b0063997198143cfeec2420dd205a2
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-02-25 09:37:25 -0300

    refactor: update layout styles and improve App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
       <p>Para visualizar o Storybook, use o comando:</p>
       <code>yarn storybook</code>
-    </>
+    </div>
   );
 }
 
