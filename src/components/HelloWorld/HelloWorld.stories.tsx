537912690ac10bb22100caf5386a7165f5f3299e
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-02-25 14:12:51 -0300

    feat: add HelloWorld component with Storybook stories and update TypeScript configuration

--- src/components/HelloWorld/HelloWorld.stories.tsx
+++ src/components/HelloWorld/HelloWorld.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import HelloWorld from "../components/HelloWorld";
+import HelloWorld from "./HelloWorld";
 
 const meta: Meta<typeof HelloWorld> = {
   title: "Example/HelloWorld",
